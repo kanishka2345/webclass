ace1c6887220bf89bfe210a94182f95263b77328
kanishka2345 <150791892+kanishka2345@users.noreply.github.com>
2024-03-01 11:14:14 +0530

    Add files via upload

---
+++ day9 classwork/day9 cw2/classwork/src/main/java/com/ex3/classwork/repository/RecipeRepo.java
@@ -0,0 +1,16 @@
+package com.ex3.classwork.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.ex3.classwork.model.Recipe;
+import java.util.List;
+
+
+@Repository
+public interface RecipeRepo extends JpaRepository<Recipe,Integer>{
+
+    @Query("select r from Recipe r where recipeName=?1")
+    List<Recipe> findByProduct(String name);
+    
+}
